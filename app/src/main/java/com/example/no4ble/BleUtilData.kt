e69374dca2e3924282544e65e985880cfe5391c4
you <you@example.com>
2022-11-22 06:56:19 -0800

    ble service uuid use nordic nus

--- app/src/main/java/com/example/no4ble/BleUtilData.kt
+++ app/src/main/java/com/example/no4ble/BleUtilData.kt
@@ -1,7 +1,8 @@
 package com.example.no4ble
 
 class BleUtilData {
-    private val ble_uuid = "[00000001-0002-0003-0004-1234567890ab]"
+    //private val ble_uuid = "[00000001-0002-0003-0004-1234567890ab]"
+    private val ble_uuid = "[6e400001-b5a3-f393-e0a9-e50e24dcca9e]" /* nordic nus */
 
     fun getUuid():String {
         return ble_uuid
@@ -21,9 +22,9 @@ I/printGattTable: Service 00001800-0000-1000-8000-00805f9b34fb
     |--00000002-0002-0003-0004-1234567890ab
     |--00000003-0002-0003-0004-1234567890ab
  */
-    private val svc_user = "00000001-0002-0003-0004-1234567890ab"
-    private val char_wr = "00000002-0002-0003-0004-1234567890ab" /* incoming into the peripheral */
-    private val char_rd = "00000003-0002-0003-0004-1234567890ab"
+    private val svc_user = "6e400001-b5a3-f393-e0a9-e50e24dcca9e"
+    private val char_wr = "6e400002-b5a3-f393-e0a9-e50e24dcca9e" /* incoming into the peripheral */
+    private val char_rd = "6e400003-b5a3-f393-e0a9-e50e24dcca9e"
     private val char_rd_ccc = "00002902-0000-1000-8000-00805f9b34fb"
 
     fun getCharSvc():String {
